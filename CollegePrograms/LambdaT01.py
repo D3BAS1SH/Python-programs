d8dd2d28f6beed965a15f4ecad470fc6a2e02afb
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-02-20 09:43:23 +0530

    LambdaT01 updated.

--- CollegePrograms/LambdaT01.py
+++ CollegePrograms/LambdaT01.py
@@ -1,4 +1,5 @@
 #make a dictionary where the key are the members of the list of integers, values are square of integers if it is even else cube of integers.
 KEYVALS=[10,20,1,2,3,11,13]
 D=(map(lambda x : {x:x**2} if x%2==0 else {x: x**3} ,KEYVALS))
-print(list(D))
+D0={x:(lambda v:v**2 if v%2==0 else v**3)(x) for x in KEYVALS}
+print(D0)
