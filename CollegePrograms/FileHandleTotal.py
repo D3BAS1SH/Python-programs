733d8b1ec7a3c6c4983ecd2f241cc7828942565e
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-06 13:58:32 +0530

    Created Final File Handle

--- CollegePrograms/FileHandleTotal.py
+++ CollegePrograms/FileHandleTotal.py
@@ -1,6 +1,6 @@
-FILEPATH='./CollegePrograms/CacheFiles'
 File=None
-
+Fname=''
+Exit=False
 def Options():
     print()
     print("1) Create a file.")
@@ -16,6 +16,43 @@ def GeChoice():
     return x
 
 def CreateFile():
+    global File,Fname
     Fname = input("File Name (You don't need to add .txt at the end.) : ")
-    File = open(f"{FILEPATH}/{Fname}.txt",'a+')
-    print()
+    try:
+        File = open(f"./{Fname}.txt",'a+')
+    except Exception as E:
+        print("File Creation : Failed.\nReason :",E)
+    print("File Creation : Success")
+    #File.close()
+
+def AddFriendInfo():
+    name=input("Friend Name : ")
+    nomb=input("Friend Numb : ")
+    city=input("Friend City : ")
+    try:
+        File.write(f"{name},{nomb},{city}\n")
+    except Exception as e:
+        print("Writing Status : Failed\nReason :",e)
+    print("Writing Status : Success.")
+    # File.close()
+
+def ReadAllDataFrom():
+    global File
+    File=open(f'{Fname}.txt','r')
+    print(File.read())
+
+def SearchInFile():
+    return
+
+while(not Exit):
+    match GeChoice():
+        case 1:
+            CreateFile()
+        case 2:
+            AddFriendInfo()
+        case 3:
+            ReadAllDataFrom()
+        case 4:
+            SearchInFile()
+        case 5:
+            Exit = not Exit
