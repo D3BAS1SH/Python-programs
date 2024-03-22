d56408c20760c5ccb1c1dade942fd15d6f176799
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-22 17:55:30 +0530

    ChallengeOfPartition Completed

--- CollegePrograms/ChallengeOfPartion.py
+++ CollegePrograms/ChallengeOfPartion.py
@@ -1,21 +1,28 @@
 #Write a program to keep on partitioning and make new list and store it.
-FINALL=[]
+# FINALL=[]
 
-def Divider(V,L,R):
-    global FINALL
-    MAINLIST=[]
-    if(L>=R):
-        return
-    X=(L+int(((R-L)/2)))
-    print(f"Parition L : {V[L:X+1]} R : {V[X+1:R+1]}")
-    MAINLIST=[Divider(V,L,X),Divider(V,X+1,R)]
-    print(MAINLIST)
-    FINALL.append(Divider(V,L,X))
-    FINALL.append(Divider(V,X+1,R))
-    # Divider(V,L,X)
-    # Divider(V,X+1,R)
-    return [V[L:X+1],V[X+1:R+1]]
+# def Divider(V,L,R):
+#     global FINALL
+#     MAINLIST=[]
+#     if(L>=R):
+#         return
+#     X=(L+int(((R-L)/2)))
+#     print(f"Parition L : {V[L:X+1]} R : {V[X+1:R+1]}")
+#     MAINLIST=[Divider(V,L,X),Divider(V,X+1,R)]
+#     print(MAINLIST)
+#     FINALL.append(Divider(V,L,X))
+#     FINALL.append(Divider(V,X+1,R))
+#     # Divider(V,L,X)
+#     # Divider(V,X+1,R)
+#     return [V[L:X+1],V[X+1:R+1]]
 
-Divider([1,2,3,4,5,6,7,8],0,7)
-print(FINALL)
+# Divider([1,2,3,4,5,6,7,8],0,7)
+# print(FINALL)
 # print(MAINLIST)
+
+def Divider(V):
+    if(len(V)<=1):
+        return V
+    return [Divider(V[:int(len(V)/2)]),Divider(V[int(len(V)/2):])]
+
+print(Divider([1,2,3,4,5,6,7,8]))
