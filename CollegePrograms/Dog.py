1b0240b5895dd82ffa900a6b13f3e3a4cb7edfc9
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-30 14:25:45 +0530

    A assignment with Mind Twister.

---
+++ CollegePrograms/Dog.py
@@ -0,0 +1,9 @@
+class Dog:
+    def __init__(self,name,age,color):
+        self.Name=name
+        self.Age=age
+        self.Color=color
+        pass
+
+D= Dog("Tommy",4,"Black")
+D1=Dog("Mickey",6,"Brown")
--- CollegePrograms/Student.py
+++ CollegePrograms/Student.py
@@ -26,3 +26,24 @@ Cs=Student("Smarak")
 Cs.info()
 Student.fun(S)
 Student.WhoIsOlder(S,S1).info()
+
+class Dog:
+    def __init__(self,name,age,color):
+        self.Name=name
+        self.age=age
+        self.Color=color
+        pass
+    def info(self):
+        print(f"Name : {self.Name}, Age : {self.age}, Color : {self.Color}")
+
+D= Dog("Tommy",4,"Black")
+D1=Dog("Mickey",6,"Brown")
+
+Student.WhoIsOlder(D,D1).info()
+
+#There are 3 different types of animals one dog, one cat, and mouse.
+#Cat having attribute name and private age.
+#Dog Having private name and age and color.
+#Mouse having attribute color and size in terms of length
+
+#Create two objects from each class and check whether the Dog objects have same name as Cat object and Check whether the dog objects have same color as mouse object. 
