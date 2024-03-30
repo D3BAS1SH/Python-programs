073a50f77b1660a22c9aef998a5f4301aebb9134
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-03-30 14:06:40 +0530

    Static method using.

--- CollegePrograms/Student.py
+++ CollegePrograms/Student.py
@@ -6,11 +6,18 @@ class Student:
     def info(self):
         print(f"Name : {self.Name}, Age : {self.age}")
         pass
+    @staticmethod
+    def fun():
+        print("Helloooo Studenats...!!")
 S=Student("Anjani")
 S1=Student("Anshu")
-S1.age=19
+# S1.age=19
 S.info()
 S1.info()
 Student.age=1002
 S1.info()
 S.info()
+Cs=Student("Smarak")
+Cs.info()
+Cs.fun()
+Student.fun()
