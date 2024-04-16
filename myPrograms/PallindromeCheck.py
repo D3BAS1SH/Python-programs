64de2ac4dfec101eb24471482bb0da20bf257dd4
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-16 22:33:54 +0530

    palindrome Checking improved.

--- myPrograms/PallindromeCheck.py
+++ myPrograms/PallindromeCheck.py
@@ -1,5 +1,5 @@
 mainStr=input("A String : ")
-if mainStr == mainStr[::-1]:
+if mainStr.lower() == mainStr[::-1].lower():
     print(f"{mainStr} is pallindrome sentence.")
 else:
     print(f"{mainStr} is not pallindrome sentence.")
