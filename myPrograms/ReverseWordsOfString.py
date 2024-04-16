f219e9db98657d99cb2f5a6ff5280b4d96a9257e
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-17 01:52:24 +0530

    ReverseWordsOfString modified with a better space consumption.

--- myPrograms/ReverseWordsOfString.py
+++ myPrograms/ReverseWordsOfString.py
@@ -1,3 +1,3 @@
 Sentence = input("Any sentence : ")
-RevWords=[x[::-1] for x in Sentence.split(' ')]
-print((" ".join(RevWords)))
+# RevWords=
+print((" ".join([x[::-1] for x in Sentence.split(' ')])))
