cfe6f70059d49cb3334f9f3d27e1c05607d71202
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-20 00:06:21 +0530

    Read Word by word edited. to read from lines and then read from word of the lines.

--- myPrograms/FileReadWordByWord.py
+++ myPrograms/FileReadWordByWord.py
@@ -1,2 +1,8 @@
-with open('RandomText.txt',"r+") as F:
-    print(F.read())
+with open('./myPrograms/RandomText.txt',"r+") as F:
+    
+    print("****************")
+    for x in F:
+        """ print(F.readline()) """
+        for word in x.split(' '):
+            print(word)
+    print("****************")
